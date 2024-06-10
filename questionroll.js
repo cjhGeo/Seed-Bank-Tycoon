5b4578225983df7c19e571020675a0966dc9610e
KING-SHUVA <130894472+KING-SHUVA@users.noreply.github.com>
2024-06-10 22:36:17 +0800

    Update questionroll.js

--- questionroll.js
+++ questionroll.js
@@ -11,12 +11,17 @@ const optionsget = document.getElementById("options");
 const nextget = document.getElementById("next");
 
 let queston = 0;
+// this is the quesiton number they are on in index form
 let score = 0;
 
 function questioning(){
+    // to display the quesitons as they come and go
     const question = quiz[queston];
+    // new variable question will be whatever quiz is on in list
     questionget.innerText = question.question;
+    // the text of the question div is the text of the question's question section
     optionsget.innerHTML = "";
+    // i'm a bit stuck
     
 
 
