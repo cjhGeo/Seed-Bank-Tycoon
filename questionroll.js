ab4253a90871f40a5754de22c88b7c65a21c42a0
KING-SHUVA <130894472+KING-SHUVA@users.noreply.github.com>
2024-06-10 22:17:12 +0800

    Update questionroll.js

---
+++ questionroll.js
@@ -0,0 +1,11 @@
+const quiz = [
+    {
+        question: "what is 1",
+        options : ["1", "2", "3", "4"],
+        answer: "2"
+        //this is merely an example and more can be added
+    },
+];
+const questionget = document.getElementById("question");
+const optionsget = document.getElementById("options");
+const nextget = document.getElementById("next");
